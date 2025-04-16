7d97e7d917b73f28f9495b56d25b186c20967623
Dang <tonminhdang9@gmail.com>
2025-04-16 22:28:11 +0700

    change 2

--- src/db.php
+++ src/db.php
@@ -1,24 +1,24 @@
 <?php
 
-$envPath = __DIR__ . '/.env';
+    $envPath = __DIR__ . '/.env';
 
-if (file_exists($envPath)) {
-    $lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) continue; // Bỏ qua dòng comment
-        list($name, $value) = explode('=', $line, 2);
-        $name = trim($name);
-        $value = trim($value);
-        putenv("$name=$value");
+    if (file_exists($envPath)) {
+        $lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) continue;
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+            putenv("$name=$value");
+        }
     }
-}
 
-$host = getenv("DB_HOST");
-$port = getenv("DB_PORT");
-$db   = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
+    $host = getenv("DB_HOST");
+    $port = getenv("DB_PORT");
+    $db   = getenv("DB_NAME");
+    $user = getenv("DB_USER");
+    $pass = getenv("DB_PASS");
 
-$conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
+    $conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
 
 ?>
